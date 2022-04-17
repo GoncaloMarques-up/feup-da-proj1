70a7cc786beba664a3f6777fdc0022f488a2f083
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2022-04-17 18:06:12 +0100

    added a way to test smaller datasets and fixed an error in cenario 3

--- src/Armazem.cpp
+++ src/Armazem.cpp
@@ -154,7 +154,7 @@ bool c3Sort(const Encomenda &encomenda1, const Encomenda &encomenda2){
 
 void Armazem::cenario3(int dia) {
     list<int> time;
-    int total = 0, current, workHours = 28800, tempoAcumulado = 0;
+    int total = 0, current, workHours = 28800, tempoAcumulado = 0, nTotal = encomendas.size();
 
     encomendas.sort(c3Sort);
     auto it = encomendas.begin();
@@ -176,8 +176,8 @@ void Armazem::cenario3(int dia) {
 
     cout << "Dia "<< dia << endl;
     cout << "Numero de Encomendas Entregues: " << tam << endl;
-    cout << "Numero de Encomendas Total: " << encomendas.size() << endl;
-    cout << "Perc. de Encomendas Entregues: " << (double) tam/encomendas.size() * 100 << "%" << endl;
-    cout << "Tempo Acumulado: " << tempoAcumulado << endl;
+    cout << "Numero de Encomendas Total: " << nTotal << endl;
+    cout << "Perc. de Encomendas Entregues: " << (double) tam/nTotal * 100 << "%" << endl;
+    cout << "Tempo Total: " << total << endl;
     cout << "Tempo Medio: "<< (double) tempoAcumulado/tam << endl << endl;
 }
